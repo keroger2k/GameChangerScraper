d6b29c44c3c56e3d3164d66049309487e1d1215b
Kyle Rogers <kroger03@harris.com>
2019-07-10 08:51:27 -0500

    ref fix

--- GameChangeScraper/Program.cs
+++ GameChangeScraper/Program.cs
@@ -12,7 +12,7 @@ namespace GameChangeScraper
             string username = "kyle.rogers@gmail.com";
             string password = "password";
             string csrf = myCookies.GetCookies(new Uri("http://gc.com"))[0].Value;
-            string postData = "email=" + username + "&password=" + password + "&csrfmiddlewaretoken=" + csrf + "&form=submit";
+            string postData = "email=" + username + "&password=" + password + "&redirect=&csrfmiddlewaretoken=" + csrf + "&form=submit";
 
             bool result = HttpMethods.Post("https://gc.com/login", postData, "https://gc.com", myCookies);
             if (result)
